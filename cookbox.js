f0750f3909378c2ec4ece791ac8f8186473e804a
Chirurgus <sasha.sashaxl@gmail.com>
2018-06-16 09:38:36 +0200

    Change startup message

--- cookbox.js
+++ cookbox.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 var app = express();
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Cookbox Server\nCreated by Alexander Sorochynskyi\nListening on localhost:3000');
 });
 
 var router = require('./router');
